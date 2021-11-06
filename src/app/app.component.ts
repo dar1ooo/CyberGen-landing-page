27a815f10e27f8a9c723fb1136d3e6e89802728f
dario.schaffner <darioschaffner7@gmail.com>
2021-11-06 22:06:11 +0100

    removed unnecessary content FIX video not playing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,18 @@ import { AfterViewInit, Component, Renderer2 } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class HomeComponent implements AfterViewInit {
-  title = 'CyberGen-landing-page';
-
+  public showMintingNotPossible = false;
+  public discordURL = 'https://discord.gg/3D6RN4k3FB';
   constructor(private renderer: Renderer2) {}
 
   ngAfterViewInit() {
     let loader = this.renderer.selectRootElement('#loader');
     this.renderer.setStyle(loader, 'display', 'none');
+  }
+  public showErrorMessage(): void {
+    this.showMintingNotPossible = true;
+  }
+  public openDiscord(): void {
+    window.open(this.discordURL, '_blank');
   }
 }
