d8bf5855716a320c49a0d62f40e57de9717935b5
dario.schaffner <darioschaffner7@gmail.com>
2021-11-19 02:02:19 +0100

    WIP responsive UI. added media width secific styles. Changed layout of whole Page. No where near done. Push needed to work on this at school

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class HomeComponent {
   public showMintingNotPossible = false;
   public discordURL = 'https://discord.gg/3D6RN4k3FB';
-  constructor() {}
 
   public showErrorMessage(): void {
     this.showMintingNotPossible = true;
